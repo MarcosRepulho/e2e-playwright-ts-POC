5d5e8be038319724ec6221912a397c6759b69f9f
Marcos Repulho <marcos.repulho20@gmail.com>
2026-04-22 21:12:25 -0300

    fix(playwright): ajustar projetos para Chromium apenas

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,6 @@ export default defineConfig({
     video: 'retain-on-failure',
     trace: 'on-first-retry',
   },
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
-  ],
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   outputDir: 'reports/artifacts',
 });
